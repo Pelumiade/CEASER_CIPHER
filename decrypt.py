b9b3f671d25aab4cff024d15925b8c7c8773513d
_Labbie <107613799+Pelumiade@users.noreply.github.com>
2023-03-02 00:05:55 +0100

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,36 @@
+message = input("enter the message you want to decode: ")
+shift = int(input("enter the shift value: "))
+shift_direction = input("Please enter the shift direction (Enter 'F' for forward, 'B' for backward): ")
+
+def decrypt_message(message, shift, shift_direction):
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    figure = '0123456789'
+    result = ""
+    result_figure = ""
+
+    # Determine the direction of the shift
+    if shift_direction.lower() == 'b':
+        shift = - shift   
+    #run on each letter in the message
+    for letter in message:
+        if letter.isdigit() and letter in figure:
+            index = figure.find(letter)
+            index = (index - shift) % ((len(figure)))
+            letter = (figure[index])
+            result_figure += letter
+        elif letter in alphabet:
+            index = alphabet.find(letter.lower())
+            index = (index - shift) % (len(alphabet))
+            if letter.isupper():
+                result += alphabet[index].lower()
+            elif letter.islower():
+                result += alphabet[index].upper()
+        else:
+            result += letter
+            
+    return result + result_figure
+
+# A Banking app, 
+#
+
+print(decrypt_message(message, shift, shift_direction))
